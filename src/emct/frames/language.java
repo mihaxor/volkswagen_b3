c87d0399170bf898ed3da598c818bca40f4cb86f
Catalin Glavan <glmihaicata@hotmail.com>
2016-12-26 04:04:15 +0200

    // ADD EMCT class

---
+++ src/emct/frames/language.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package emct.frames;
+
+/**
+ *
+ * @author Catalin Glavan
+ */
+public interface language {
+
+    final String sub1_txt1 = "Idle speed";
+    final String sub1_txt2 = "CO level";
+    final String sub1_txt3 = "Throttle initial position";
+
+}
